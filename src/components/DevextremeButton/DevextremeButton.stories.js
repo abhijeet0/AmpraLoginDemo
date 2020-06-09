f17d73bbdd7fb2c205c8a0fa8c7b807d4d380070
abhijeet0 <abhijeetgund0@gmail.com>
2020-06-09 19:16:12 +0530

    storybook added

---
+++ src/components/DevextremeButton/DevextremeButton.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from './DevextremeButton'
+
+export default {
+	title: "Button",
+	component: Button
+};
+
+export const DevButton = () => (
+	<Button text = "Login" />
+);
+
+
---
+++ src/components/DevextremeTextBox/DevextremeTextBox.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TextBox from './DevextremeTextBox'
+
+export default {
+	title: "TextBox",
+	component: TextBox
+};
+
+export const Text = () => (
+	<TextBox placeholder = "LoginInput" />
+);
+
+

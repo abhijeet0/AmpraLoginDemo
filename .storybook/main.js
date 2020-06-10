9bf71e64bc35cdc486994212a2e66233d38f30c9
abhijeet0 <abhijeetgund0@gmail.com>
2020-06-10 13:06:06 +0530

    storybook-addons added

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,11 @@
 module.exports = {
     stories: ['../src/**/*.stories.[tj]s'],
+    addons: [
+        // '@storybook/addon-actions',
+        '@storybook/addon-knobs',
+        '@storybook/addon-links',
+        '@storybook/addon-knobs/register',
+        '@storybook/addon-actions/register',
+        '@storybook/addon-a11y/register'
+      ],
     };
--- src/components/DevextremeButton/DevextremeButton.stories.js
+++ src/components/DevextremeButton/DevextremeButton.stories.js
@@ -1,13 +1,53 @@
 import React from "react";
-import Button from './DevextremeButton'
+import Button from "./DevextremeButton";
+import {
+  withKnobs,
+  text,
+  boolean,
+  number,
+  select,
+  object,
+} from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { withA11y } from "@storybook/addon-a11y";
+import { addDecorator } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+
+addDecorator(withInfo);
 
 export default {
-	title: "Button",
-	component: Button
+  title: "Button",
+  component: Button,
+  decorators: [withKnobs, withA11y, withInfo],
 };
 
 export const DevButton = () => (
-	<Button text = "Login" />
+  <Button
+    onClick={action("button-click")}
+    text={text("Label", "Login")}
+    disabled={boolean("Disabled", false)}
+  />
 );
 
+export const accessible = () => (
+  <Button
+    onClick={action("accessible-click")}
+    text={text("Label", "Login")}
+    disabled={boolean("Disabled", false)}
+  />
+);
 
+export const inaccessible = () => (
+  <Button
+    style={{ backgroundColor: "red", color: "darkRed" }}
+    onClick={action("inaccessible-click")}
+    text={text("Label", "Login")}
+    disabled={boolean("Disabled", false)}
+  />
+);
+
+DevButton.story = {
+  parameters: {
+    info: { inline: true },
+  },
+};
